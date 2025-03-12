161ae30008b6def14490bdd890ef136bb46df515
Magiktrekbean <153703739+Magiktrekbean@users.noreply.github.com>
2025-03-11 21:09:34 -0400

    Initial commit

---
+++ src/Cities.js
@@ -0,0 +1,14 @@
+import react from "react";
+export default function Cities() {
+  return (
+    <div className="Cities">
+      <ul>
+        <li>Sydney</li>
+        <li>Paris</li>
+        <li>Montreal</li>
+        <li>Lisbon</li>
+        <li>San Francisco</li>
+      </ul>
+    </div>
+  );
+}
